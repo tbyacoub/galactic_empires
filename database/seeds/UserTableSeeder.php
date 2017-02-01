c8b0671ba02a02ff03f2f8b208400f60520e94a1
Tarek Yacoub <tbyacoub@iastate.edu>
2017-02-01 11:20:51 -0600

    fixed userTableSeeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -4,6 +4,10 @@ use Illuminate\Database\Seeder;
 
 class UserTableSeeder extends Seeder
 {
+
+    private $player;
+
+    private $admin;
     /**
      * Run the database seeds.
      *
@@ -11,11 +15,12 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        $admin = \App\Role::where('name', 'admin')->get()[0];
+        $this->admin = \App\Role::where('name', 'admin')->get()[0];
+        $this->player = \App\Role::where('name', 'player')->get()[0];
 
         factory(\App\User::class, 50)->create()->each(function ($u){
-            $player = \App\Role::where('name', 'player')->get()[0];
-            $u->attachRole($player);
+//            $player = \App\Role::where('name', 'player')->get()[0];
+            $u->attachRole($this->player);
         });
 
         $adminUser = new \App\User();
@@ -23,6 +28,13 @@ class UserTableSeeder extends Seeder
         $adminUser->email = 'tbyacoub@gmail.com';
         $adminUser->password = (bcrypt('password'));
         $adminUser->save();
-        $adminUser->attachRole($admin);
+        $adminUser->attachRole($this->admin);
+
+        $playerUser = new \App\User();
+        $playerUser->name = 'test';
+        $playerUser->email = 'test@test.com';
+        $playerUser->password = bcrypt('password');
+        $playerUser->save();
+        $playerUser->attachRole($this->player);
     }
 }
