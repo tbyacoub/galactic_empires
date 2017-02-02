94272e346dedfaba0f644a8b790bf0bc8db1c150
Tarek Yacoub <tbyacoub@iastate.edu>
2017-02-01 18:48:25 -0600

    retrieved assests

--- resources/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-// Script for toggling active class on li items in
-
-$(".nav li").on("click", function() {
-    console.log(this);
-    $(".nav li").removeClass("active");
-    $(this).addClass("active");
-});
