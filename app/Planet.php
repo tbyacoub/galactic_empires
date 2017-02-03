6676c756b71641c2422bd13fd0d80147da7399f9
Tarek Yacoub <tbyacoub@iastate.edu>
2017-02-03 15:45:51 -0600

    Added fillable attrabuites to (Planet, PlanetType, SolarSystem) models

--- app/Planet.php
+++ app/Planet.php
@@ -8,6 +8,15 @@ class Planet extends Model
 {
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'radius', 'resources',
+    ];
+
+    /**
      * The attributes that should be casted to native types.
      *
      * @var array
--- app/SolarSystem.php
+++ app/SolarSystem.php
@@ -8,6 +8,15 @@ class SolarSystem extends Model
 {
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'max_planets', 'location',
+    ];
+
+    /**
      * The attributes that should be casted to native types.
      *
      * @var array
