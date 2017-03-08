bb800920106d711528623cec0fb218fc46470fca
Jorefice <jorefice@iastate.edu>
2017-03-08 13:08:26 -0600

    Moved update resources to job

--- app/Console/Commands/UpdateResources.php
+++ app/Console/Commands/UpdateResources.php
@@ -38,46 +38,9 @@ class UpdateResources extends Command
      */
     public function handle()
     {
-       $planets = \App\Planet::get();
-       $global_multiplier = 1;
-       $bonus = 1;
-       foreach ($planets as $planet)
-       {
-          // echo $planet->name;
-           $metal = $planet->metal();
-           $crystal = $planet->crystal();
-           $energy = $planet->energy();
-           $buildings = $planet->buildings();
-           foreach($buildings as $building) 
-           {
-              // $resources = $building->products()->where('producible_type', 'App\Building')->get();
-            if($building->producible_type == 'App\Building')
-            {
-               $metal += (($building->characteristics['metal_base_rate']) * $bonus);
-               $crystal += (($building->characteristics['crystal_base_rate']) * $bonus);
-               $energy += (($building->characteristics['energy_base_rate']) * $bonus);
-            }
-              foreach ($resources as $resourceBuilding)
-              {
-                // $bonus = (1 + (0.25 * (1 - $resourceBuilding->current_level))) * $global_multiplier;  //calculate production rate bonus as a function of current level and global miltiplier
-                $metal += (($resourceBuilding->characteristics['metal_base_rate']) * $bonus);
-                $crystal += (($resourceBuilding->characteristics['crystal_base_rate']) * $bonus);
-                $energy += (($resourceBuilding->characteristics['energy_base_rate']) * $bonus);
-              }
-              $planet->resources = [
-                    'metal' => 5,
-                    'crystal' => 5,
-                    'energy' => 5
-              ];
-           }
-          //  $planet->resources = [
-          //         'metal' => $metal,
-          //         'crystal' => $crystal,
-          //         'energy' => $energy
-          //       ];
-          // $planet->save();
-       }
+      dispatch(new \App\Jobs\UpdateResources());
     }
+
 }
 
 
--- app/Jobs/UpdateResources.php
+++ app/Jobs/UpdateResources.php
@@ -17,7 +17,7 @@ class UpdateResources implements ShouldQueue
      *
      * @return void
      */
-    public function __construct(Planet $planet, User $user)
+    public function __construct()
     {
         //
     }
@@ -29,6 +29,34 @@ class UpdateResources implements ShouldQueue
      */
     public function handle()
     {
+       $planets = \App\Planet::get();
+       $bonus = 1;
+       $global =  \App\GlobalRate::first();
+       foreach ($planets as $planet)
+       {
+          // echo $planet->name;
+           $metal = $planet->metal();
+           $crystal = $planet->crystal();
+           $energy = $planet->energy();
+           $buildings = $planet->buildings()->get();
+           foreach($buildings as $building) 
+           {
+              $resources = $building->products()->where('producible_type', 'App\Building')->get();
+              foreach ($resources as $resourceBuilding)
+              {
+                $bonus = (1 + (0.25 * (1 - $resourceBuilding->current_level)));  //calculate production rate bonus as a function of current level and global miltiplier, divide by 12 to convert hourly rate to every 5 minutes
+                $metal += (($resourceBuilding->characteristics['metal_base_rate']) * $bonus * $global->mineral_rate) / 12;
+                $crystal += (($resourceBuilding->characteristics['crystal_base_rate']) * $bonus * $global->crystal_rate) / 12;
+                $energy += (($resourceBuilding->characteristics['energy_base_rate']) * $bonus * $global->energy_rate) / 12;
+              }
+              $planet->resources = [
+                    'metal' => ceil($metal),
+                    'crystal' => ceil($crystal),
+                    'energy' => ceil($energy)
+              ];
+              $planet->save();
+           }
+       }   
     }
        
 }
