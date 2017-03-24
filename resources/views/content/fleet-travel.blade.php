4028e0ca0b95a37e9e867adaacda1bba7fd1e3c7
anegrete <anegrete@iastate.edu>
2017-03-23 20:44:23 -0500

    Launch attack logic complete, combied with Travel, & notifications

--- resources/views/content/fleet-travel.blade.php
+++ resources/views/content/fleet-travel.blade.php
@@ -7,7 +7,7 @@
     <table class="table table-striped table-advance table-hover">
         <thead>
             <tr>
-                <th>Fleet</th>
+                <th>Your Fleet</th>
                 <th>Image</th>
                 <th>Our Planet</th>
                 <th>Duration</th>
@@ -19,17 +19,17 @@
         <tbody>
         @foreach($from_travels as $travel)
             <tr>
-                <td>{{ $travel->fleet[0]['type'] }} : {{ $travel->fleet[0]['amount'] }}</td>
-                <td><img src="{{ $travel->fromPlanet()->first()->img_path }}" alt="Planet Image" style="width:75px;height:75px;"></td>
+                <td></td>
+                <td><img src="{{ $travel->fromPlanet()->first()->PlanetType->first()->img_path }}" alt="Planet Image" style="width:75px;height:75px;"></td>
                 <td>{{ $travel->fromPlanet()->first()->name}}</td>
                 <td width="40%">
                     <p>Arrives on {{ $travel->arrival }}</p>
                     <div class="progress progress-striped active">
-                        <div class="progress-bar"  role="progressbar" style="width: {{$travel->getTravelPercent()}}%"> </div>
+                        <div data-rate="{{ $travel->getPercentRatePerSecond() }}" data-width=" {{$travel->getTravelPercent()}}" class="progress-bar from-travel-pb"  role="progressbar" style="width: {{$travel->getTravelPercent()}}%"> </div>
                     </div>
                 </td>
                 <td>{{ $travel->toPlanet()->first()->name}}</td>
-                <td><img src="{{ $travel->toPlanet()->first()->img_path }}" alt="Planet Image" style="width:75px;height:75px;"></td>
+                <td><img src="{{ $travel->toPlanet()->first()->PlanetType->first()->img_path }}" alt="Planet Image" style="width:75px;height:75px;"></td>
             </tr>
         @endforeach
         </tbody>
@@ -57,16 +57,16 @@
         <tbody>
         @foreach($to_travels as $travel)
             <tr>
-                <td><img src="{{ $travel->toPlanet()->first()->img_path }}" alt="Planet Image" style="width:75px;height:75px;"></td>
+                <td><img src="{{ $travel->toPlanet()->first()->PlanetType->first()->img_path }}" alt="Planet Image" style="width:75px;height:75px;"></td>
                 <td>{{ $travel->toPlanet()->first()->name}}</td>
                 <td width="40%">
                     <p>Arrives on {{ $travel->arrival }}</p>
                     <div class="progress progress-striped active" style="transform: scaleX(-1)">
-                        <div class="progress-bar progress-bar-danger"  role="progressbar" style="width: {{$travel->getTravelPercent()}}%"> </div>
+                        <div data-rate="{{ $travel->getPercentRatePerSecond() }}" data-width=" {{$travel->getTravelPercent()}}" class="progress-bar progress-bar-danger from-travel-pb"  role="progressbar" style="width: {{$travel->getTravelPercent()}}%"> </div>
                     </div>
                 </td>
                 <td>{{ $travel->fromPlanet()->first()->name}}</td>
-                <td><img src="{{ $travel->fromPlanet()->first()->img_path }}" alt="Planet Image" style="width:75px;height:75px;"></td>
+                <td><img src="{{ $travel->fromPlanet()->first()->PlanetType->first()->img_path }}" alt="Planet Image" style="width:75px;height:75px;"></td>
             </tr>
         @endforeach
         </tbody>
