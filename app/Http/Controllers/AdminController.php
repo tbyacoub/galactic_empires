06c25f1271f7f111f679c8af7d4e5381e0a47078
Tarek Yacoub <tbyacoub@iastate.edu>
2017-04-18 21:52:01 -0500

    view name fix

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -24,7 +24,7 @@ class AdminController extends Controller
      */
     public function globalRates(){
         $globals = GlobalRate::first()->get();
-        return view('admin.game-SETTINGS', compact('globals'));
+        return view('admin.game-settings', compact('globals'));
     }
 
     /**
